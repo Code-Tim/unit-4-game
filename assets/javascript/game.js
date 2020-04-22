7dbf1b8b386648b266d6a8093ad343573cd371b1
Tim <nicoleandtim2010@gmail.com>
2020-04-22 18:15:16 -0400

    added the ruby click function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,6 +11,7 @@ $('#numberWins').text(wins);
 $('#numberLoses').text(loses);
 // var randomNumber = []
 var totalScore = 0
+var score = 0
 
 
 // Need to select random start number each time the game starts
@@ -21,12 +22,24 @@ $('#randomNumber').text(randomNumber);
 
 
 // need to set up a random number for each crystal
-var num1 = Math.floor(Math.random() * 11 + 1)
-var num2 = Math.floor(Math.random() * 11 + 1)
-var num3 = Math.floor(Math.random() * 11 + 1)
-var num4 = Math.floor(Math.random() * 11 + 1)
+var num1 = Math.floor(Math.random() * 11 + 1);
+var num2 = Math.floor(Math.random() * 11 + 1);
+var num3 = Math.floor(Math.random() * 11 + 1);
+var num4 = Math.floor(Math.random() * 11 + 1);
 
 // need to set up a listener for the click
+$('#ruby').on('click', function () {
+    score = score + num1;
+    console.log("total score= " + score);
+    $('#totalScore').text(score);
+
+    if (score == randomNumber) {
+        winner();
+    }
+    else if (score > randomNumber) {
+        loser();
+    }
+})
 
 // add wins and losses to gamecount
 
@@ -51,28 +64,11 @@ function loser() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // set up click for crystals
 $('.crystal').on('click', function () {
     // console.log(this.innerText)
-    // console.log($(this).attr("id"))
-    console.log($(this).attr("data-factoid"))
+    console.log($(this).attr("id"))
+    // console.log($(this).attr("data-factoid"))
     // console.log($(this).data("factoid"))
 })
 
